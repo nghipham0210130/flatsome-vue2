1b33bd6ac8d0d0896e10ae0b3a4f4aa445bf3e21
Lisa <phamtonghicoder96@gmail.com>
2021-07-10 02:24:49 +0700

    perf(Login) change to use v-form to vee-validate in Login
    change to use v-form to vee-validate in Login
    
    Close

--- flatsome-spa/src/views/components/Login.vue
+++ flatsome-spa/src/views/components/Login.vue
@@ -1,100 +1,120 @@
 <template>
   <!-- Modal Login -->
   <div id="loginModal" class="modal" :key="componentKey">
-    <button tupe="button" class="modal__icon icon__close" @click="closeModal()">X</button>
+    <button tupe="button" class="modal__icon icon__close" @click="closeModal()">
+      X
+    </button>
     <div class="modal__content">
-      <!-- Form Login -->
-      <form id="loginForm" @submit.prevent="login">
+      <validation-observer v-slot="{ handleLogin }">
         <h4 class="login__title">Login</h4>
-        <label for="emailLogin">Username or email address*</label>
-        <span class="text__danger" v-if="emailErrorLogin != null">{{
-          emailErrorLogin
-        }}</span>
-        <input
-          v-model="formLogin.email"
-          type="email"
-          id="emailLogin"
-          name="emailLogin"
-        />
-        <label for="passwordLogin">Password*</label>
-        <span class="text__danger" v-if="passwordErrorLogin != null">{{
-          passwordErrorLogin
-        }}</span>
-        <input
-          v-model="formLogin.password"
-          type="password"
-          id="passwordLogin"
-          name="passwordLogin"
-        />
-        <label class="remember__user" for="isRemember">
-          Remember me
-          <input
-            v-model="formLogin.isRemember"
-            type="checkbox"
-            id="isRemember"
-            value="Remember"
-          />
-          <span class="checkmark"></span>
-        </label>
-        <button type="submit" class="btn__signin" @click.prevent="login()">
-          Log in
-        </button>
-        <a href="#">Lost your password?</a>
-      </form>
+        <!-- Form Login -->
+        <form id="loginForm" @submit.prevent="handleLogin(login)">
+          <validation-provider
+            name="e-mail"
+            rules="required|email"
+            v-slot="{ errors }"
+          >
+            <label>Username or email address*</label>
+            <span class="text__danger">{{ errors[0] }}</span>
+            <span class="text__danger" v-if="emailErrorLogin != null">{{
+              emailErrorLogin
+            }}</span>
+            <input v-model="formLogin.email" type="email" />
+          </validation-provider>
+          <validation-provider
+            name="password"
+            rules="required|min:6"
+            v-slot="{ errors }"
+          >
+            <label>Password*</label>
+            <span class="text__danger">{{ errors[0] }}</span>
+            <span class="text__danger" v-if="passwordErrorLogin != null">{{
+              passwordErrorLogin
+            }}</span>
+            <input
+              v-model="formLogin.password"
+              type="password"
+            />
+          </validation-provider>
+          <label class="remember__user" for="isRemember">
+            Remember me
+            <input
+              v-model="formLogin.isRemember"
+              type="checkbox"
+              id="isRemember"
+              value="Remember"
+            />
+            <span class="checkmark"></span>
+          </label>
+          <button type="submit" class="btn__signin" @click.prevent="login()">
+            LogIn
+          </button>
+          <a href="#">Lost your password?</a>
+        </form>
+      </validation-observer>
       <hr />
-      <!-- Form Register -->
-      <form id="registerForm" @submit.prevent="register">
+      <validation-observer v-slot="{ handleRegister }">
         <h4 class="register__title">Register</h4>
-        <label for="emailRegister">Email address*</label>
-        <span class="text__danger" v-if="emailErrorRegister != null">{{
-          emailErrorRegister
-        }}</span>
-        <input
-          v-model="formRegister.email"
-          type="email"
-          id="emailRegister"
-          name="emailRegister"
-        />
-        <label for="passwordRegister">Password*</label>
-        <span class="text__danger" v-if="passwordErrorRegister != null">{{
-          passwordErrorRegister
-        }}</span>
-        <input
-          v-model="formRegister.password"
-          type="password"
-          id="passwordRegister"
-          name="passwordRegister"
-        />
-        <p>
-          Your personal data will be used to support your experience throughout
-          this website, to manage access to your account, and for other purposes
-          described in our privacy policy.
-        </p>
-        <button type="submit" class="btn__register" @click.prevent="register()">
-          Register
-        </button>
-      </form>
+        <!-- Form Register -->
+        <form id="registerForm" @submit.prevent="handleRegister(register)">
+          <validation-provider
+            name="e-mail"
+            rules="required|email"
+            v-slot="{ errors }"
+          >
+            <label>Email address*</label>
+            <span class="text__danger">{{ errors[0] }}</span>
+            <span class="text__danger" v-if="emailErrorRegister != null">{{
+              emailErrorRegister
+            }}</span>
+            <input v-model="formRegister.email" type="email" />
+          </validation-provider>
+          <validation-provider
+            name="password"
+            rules="required|min:6"
+            v-slot="{ errors }"
+          >
+            <label>Password*</label>
+            <span class="text__danger">{{ errors[0] }}</span>
+            <input
+              v-model="formRegister.password"
+              type="password"
+            />
+          </validation-provider>
+          <p>
+            Your personal data will be used to support your experience
+            throughout this website, to manage access to your account, and for
+            other purposes described in our privacy policy.
+          </p>
+          <button
+            type="submit"
+            class="btn__register"
+            @click.prevent="register()"
+          >
+            Register
+          </button>
+        </form>
+      </validation-observer>
     </div>
   </div>
 </template>
 
 <script>
 import { mapState, mapActions, mapMutations } from "vuex";
-import Form from "vform";
 
 export default {
   data() {
     return {
       componentKey: 0,
-      formLogin: new Form({
+      formLogin: {
         email: "",
         password: "",
         isRemember: false,
-      }),
-      formRegister: new Form({
+      },
+      formRegister: {
         email: "",
         password: "",
-      }),
+      },
       emailErrorLogin: "",
       passwordErrorLogin: "",
       emailErrorRegister: "",
@@ -112,7 +132,7 @@ export default {
     ...mapMutations("AUTH", {
       closeModalLogin: "SHOW_MODAL_LOGIN",
       setPassword: "SET_PASSWORD",
-    })
+    }),
   },
   methods: {
     ...mapActions("AUTH", {
@@ -126,7 +146,7 @@ export default {
         password: this.formLogin.password,
         isRemember: this.formLogin.isRemember,
       });
-      // Check login not success
+      // Check login success
       if (!this.isLoggedIn) {
         //  Check errors email correctly
         if (this.errorsFromStore.username) {
@@ -145,6 +165,7 @@ export default {
         this.formLogin.email = null;
         this.formLogin.password = null;
         this.formLogin.isRemember = false;
+        location.reload();
         this.componentKey++;
       }
     },
@@ -167,13 +188,14 @@ export default {
       } else {
         this.formLogin.email = null;
         this.formLogin.password = null;
+        location.reload();
         this.componentKey++;
       }
     },
     closeModal() {
       this.closeModalLogin;
       this.componentKey++;
-    }
+    },
   },
 };
 </script>
@@ -194,11 +216,11 @@ export default {
   .modal__icon {
     &.icon__close {
       padding-top: 10px;
-      margin-bottom: 245px; 
+      margin-bottom: 245px;
       font-size: 2em;
-      border : none;
+      border: none;
       color: rgb(255, 255, 255);
-      background-color: transparent; 
+      background-color: transparent;
       margin-left: 97%;
     }
   }
@@ -214,6 +236,7 @@ export default {
     transform: translateZ(0);
     background-color: #fff;
     h4 {
+      font-size: 2em;
       text-transform: uppercase;
       color: rgb(85, 85, 85);
       font-weight: bold;
